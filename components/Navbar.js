38cfdee55a725ae30429d29ca7bccc6a817aed99
hemanthnvd <hemanthnvd@gmail.com>
2022-07-12 18:52:08 +0530

    my first commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-red-500">
+        <h1 className="text-red-50">Hello world</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
