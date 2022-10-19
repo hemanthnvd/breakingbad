db3a3d51f818e33c9c081cbc4292fd96d165870d
hemanthnvd <hemanthnvd@gmail.com>
2022-10-19 19:15:05 +0530

    added favCard

---
+++ components/ui/FavCard.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+
+const FavCard = (props) => {
+  const [temp, setTemp] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      let res = await fetch(`https://breakingbadapi.com/api/characters/${props.id}`);
+      res = await res.json();
+      setTemp(res[0]);
+    };
+    getData();
+  }, []);
+  return (
+    <>
+      <CharacterCard char_id={props.id} name={temp?.name} img={temp?.img} />
+    </>
+  );
+};
+
+export default FavCard;
--- pages/favourites.jsx
+++ pages/favourites.jsx
@@ -3,24 +3,32 @@ import { doc, getDoc } from "firebase/firestore";
 import { useAuth } from "../components/Auth/AuthContext";
 import { db } from "../src/firebase-config";
 import { useState } from "react";
+import FavCard from "../components/ui/FavCard";
 
 const Favourites = () => {
   const { user } = useAuth();
   const [favArray, setFavArray] = useState([]);
-  const getData = async () => {
-    const userSnap = await getDoc(doc(db, "users", user.uid));
-    if (userSnap.exists()) {
-      setFavArray(userSnap.data().fav);
-    } else {
-      console.log("No favourites yet!");
-    }
-  };
-  getData();
+  useState(() => {
+    const getData = async () => {
+      const userSnap = await getDoc(doc(db, "users", user.uid));
+      if (userSnap.exists()) {
+        setFavArray(userSnap.data().fav);
+      } else {
+        console.log("No favourites yet!");
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <>
       <Navbar />
-      {favArray}
+      <h1>Your Favourite Characters</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {favArray.map((id) => (
+          <FavCard id={id} />
+        ))}
+      </div>
     </>
   );
 };
