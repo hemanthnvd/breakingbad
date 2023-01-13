b6b9bddd70e5e4157da966d4333651c1517fe620
hemanthnvd <hemanthnvd@gmail.com>
2023-01-13 16:20:46 +0530

    testing first character

---
+++ pages/api/characters/1.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    char_id: 1,
+    name: "Walter White",
+    birthday: "09-07-1958",
+    occupation: ["High School Chemistry Teacher", "Meth King Pin"],
+    img: "https://web.archive.org/web/20220811172853/https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+    status: "Presumed dead",
+    nickname: "Heisenberg",
+    appearance: [1, 2, 3, 4, 5],
+    portrayed: "Bryan Cranston",
+    category: "Breaking Bad",
+    better_call_saul_appearance: [],
+  });
+}
